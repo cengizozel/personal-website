85596e25a9281f12a50bb64b98024aeb4e2d0b94
cengizozel <cozel@u.rochester.edu>
2023-08-23 12:34:59 -0400

    CSS vars, footer dark mode bg

--- js/script.js
+++ js/script.js
@@ -1,48 +1,57 @@
-const checkbox = document.getElementById("checkbox");
-let currentTheme = localStorage.getItem('theme') || 'light';
-document.body.className = currentTheme;
-checkbox.checked = (currentTheme === 'dark');
+document.addEventListener('DOMContentLoaded', function() {
 
-const body = document.body;
-const bodyStyle = body.style;
-const ball = document.querySelector('.checkbox-label .ball');
-const ballStyle = ball.style;
-const occupation = document.getElementById('occupation');
+    const checkbox = document.getElementById("checkbox");
+    let currentTheme = localStorage.getItem('theme') || 'light';
+    document.body.className = currentTheme;
+    checkbox.checked = (currentTheme === 'dark');
 
-if (checkbox.checked === true) {
-    ballStyle.transform = 'translateX(24px)';
-    occupation.textContent = "MS Student";
-} else {
-    ballStyle.transform = 'translateX(0)';
-    occupation.textContent = "Researcher";
-}
 
-function changeTheme() {
-    bodyStyle.transition = "background 0.2s linear";
-    ballStyle.transition = "transform 0.2s linear";
-    if (currentTheme === 'dark') {
-        ballStyle.transform = "translateX(24px)";
-    } else {
-        ballStyle.transform = "translateX(0)";
-    }
-}
+    const body = document.body;
+    const bodyStyle = body.style;
+    const footer = document.getElementsByTagName('footer')[0]
+    const footerStyle = footer.style;
 
-function changeOccupation() {
-    if (currentTheme === 'dark') {
+    const ball = document.querySelector('.checkbox-label .ball');
+    const ballStyle = ball.style;
+    const occupation = document.getElementById('occupation');
+
+    if (checkbox.checked === true) {
+        ballStyle.transform = 'translateX(24px)';
         occupation.textContent = "MS Student";
     } else {
+        ballStyle.transform = 'translateX(0)';
         occupation.textContent = "Researcher";
     }
-}
 
-checkbox.addEventListener('change', function () {
-    if (currentTheme === 'light') {
-        currentTheme = 'dark';
-    } else {
-        currentTheme = 'light';
+    function changeTheme() {
+        bodyStyle.transition = "background 0.2s linear";
+        ballStyle.transition = "transform 0.2s linear";
+        footerStyle.transition = "background 0.2s linear";
+        if (currentTheme === 'dark') {
+            ballStyle.transform = "translateX(24px)";
+        } else {
+            ballStyle.transform = "translateX(0)";
+        }
     }
-    document.body.className = currentTheme;
-    localStorage.setItem('theme', currentTheme);
-    changeTheme();
-    changeOccupation();
+
+    function changeOccupation() {
+        if (currentTheme === 'dark') {
+            occupation.textContent = "MS Student";
+        } else {
+            occupation.textContent = "Researcher";
+        }
+    }
+
+    checkbox.addEventListener('change', function () {
+        if (currentTheme === 'light') {
+            currentTheme = 'dark';
+        } else {
+            currentTheme = 'light';
+        }
+        document.body.className = currentTheme;
+        localStorage.setItem('theme', currentTheme);
+        changeTheme();
+        changeOccupation();
+    });
+
 });
